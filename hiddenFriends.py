1f5e29aa7f6a0ba5241828b2f3adc6d277fed267
gedon <sasessw2@gmail.com>
2019-12-11 15:10:45 +0300

    not working commit

--- hiddenFriends.py
+++ hiddenFriends.py
@@ -6,7 +6,7 @@ import time
 def id_friends(id,token):
 	link = "https://api.vk.com/method/friends.get?user_id={}&access_token={}&v=5.52".format(id,token)
 	request = requests.get(link).json()
-	
+
 	try:
 		friends_list = request ['response']['items']
 		print(friends_list)
@@ -17,6 +17,12 @@ def id_friends(id,token):
 	except KeyError:
 		print("skip unreadble json")
 		return []
+
+
+def percent_of_unique_friends(len_big_family, len_new_big_family):
+	print("all quantity: ", len_big_family)
+	print("unique quantity: ", len_new_big_family)
+	print("percent: ", 100*len_new_big_family/len_big_family)
 
 
 def main():
@@ -33,11 +39,7 @@ def main():
 
 	print("********************")
 	new_big_family = list(dict.fromkeys(big_family))
-	
-	print(new_big_family)
-	print(len(big_family))
-	print(len(new_big_family))
-	print(100*len(new_big_family)/len(big_family))
+	percent_of_unique_friends(len(big_family),len(new_big_family))
 
 
 if __name__ == "__main__":
