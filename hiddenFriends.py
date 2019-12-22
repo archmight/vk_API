a7ece8f740cc24ce171453f47be3a581273c3f42
gedon <sasessw2@gmail.com>
2019-12-22 08:27:59 +0300

    stable work

--- hiddenFriends.py
+++ hiddenFriends.py
@@ -11,11 +11,12 @@ def id_friends(id,token):
 		friends_list = request ['response']['items']
 		print(friends_list)
 		print(type(friends_list))
-		time.sleep(1)
+		time.sleep(0.34)
 		return friends_list
 
 	except KeyError:
-		print("skip unreadble json")
+		print(request)
+		time.sleep(0.34)
 		return []
 
 
@@ -23,6 +24,46 @@ def percent_of_unique_friends(len_big_family, len_new_big_family):
 	print("all quantity: ", len_big_family)
 	print("unique quantity: ", len_new_big_family)
 	print("percent: ", 100*len_new_big_family/len_big_family)
+
+
+
+def get_dict_friend_id_and_his_friend_list(friend_list, token):
+
+	dict_of_all_friends = {}
+
+	for _ in friend_list:
+		id_list = id_friends(_,token)
+		dict_of_all_friends[_] = id_list
+	return dict_of_all_friends
+
+
+def get_list_possible_hidden_friends(id,friends_dict):
+	
+	id = int(id)
+	list_of_possible_hidden_friends = []
+
+	print("all clique 3 friends")
+
+	for key,value in friends_dict.items():
+		if(id in value):
+			print("person_id:", key)
+			list_of_possible_hidden_friends.append(key)
+
+	return list_of_possible_hidden_friends
+
+
+def get_hidden_friends(id_friend_list, possible_hidden_friend_list):
+	
+	friend_list = []
+	
+	for _ in possible_hidden_friend_list:
+		if not( _ in id_friend_list):
+			friend_list.append(_)
+
+	print("numbers of hidden friends: ", len(friend_list))
+
+	for _ in friend_list:
+		print("person_id: ", _ )
 
 
 def main():
@@ -40,6 +81,13 @@ def main():
 	print("********************")
 	new_big_family = list(dict.fromkeys(big_family))
 	percent_of_unique_friends(len(big_family),len(new_big_family))
+	
+	friends_dict = get_dict_friend_id_and_his_friend_list(new_big_family,token)
+	print("********************")
+	
+	possible_hidden_friend_list = get_list_possible_hidden_friends(id,friends_dict)
+	get_hidden_friends(friend_list, possible_hidden_friend_list)
+
 
 
 if __name__ == "__main__":
